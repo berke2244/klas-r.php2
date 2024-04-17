3224f96b6cc07cb8e1999c6949c9be3713216d6f
berke2244 <146707551+berke2244@users.noreply.github.com>
2024-04-17 14:26:39 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+ <div>
+ <form action="kayit.php" method="get">
+ <textarea name="notlar" rows="4" cols="50">
+        Notunuzu buraya yazınız.
+ </textarea>
+ <br>
+ <input type="submit" value="Kaydet">
+ </form>
+ <div>
+</body>
+</html>
